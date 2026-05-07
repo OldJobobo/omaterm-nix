0ff5bf8db3bd8926b21ee443ea3e8eb33c5b0799
Ryan Hughes <ryan@heyoodle.com>
2026-05-06 22:29:39 -0400

    Use DEB_ARCH var since we have it

--- install/debian.sh
+++ install/debian.sh
@@ -49,7 +49,7 @@ install_packages() {
   if ! command -v gh &>/dev/null; then
     section "Installing GitHub CLI..."
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
+    echo "deb [arch=${DEB_ARCH} signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
     sudo apt-get update
     sudo apt-get install -y gh
   fi
